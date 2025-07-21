28190b98e2371181e8b70e50d127db6846fc523a
guillermo.parada <guillermo.parada@pragma.com.co>
2025-07-21 18:09:32 -0500

    virtual machines added

---
+++ virtual-machines/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "vm_username" {
+  type = string
+}
+
+variable "vm_password" {
+  type = string
+}
